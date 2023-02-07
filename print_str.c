e7d93b8db3322e7b11da51e320bfab85023f7bf8
Beomjin Na <bena@c3r2s7.42seoul.kr>
2023-02-08 05:31:39 +0900

    FEAT: Add print_str.c

--- print_str.c
+++ print_str.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_char.c                                       :+:      :+:    :+:   */
+/*   print_str.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bena <bena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/13 05:26:20 by bena              #+#    #+#             */
-/*   Updated: 2023/02/13 05:28:26 by bena             ###   ########.fr       */
+/*   Created: 2023/02/08 02:19:17 by bena              #+#    #+#             */
+/*   Updated: 2023/02/13 06:17:46 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,40 @@ void		*ft_memset(void *b, int c, size_t len);
 int			ft_atoi(const char *str);
 void		set_flags(const char c, t_flags *flags);
 int			is_flag_character(const char c);
-static void	put_result(char c, t_flags *flags, size_t width);
+static void	put_result(char *s, t_flags *flags, size_t width, size_t precision);
+size_t		ft_strlen(const char *s);
+size_t		min_len(size_t a, size_t b);
+void		ft_putstrn(char *s, size_t buffer);
 
-int	print_char(char c, const char *ptr)
+int	print_str(char *str, const char *ptr)
 {
 	t_flags	flags;
 	size_t	width;
+	size_t	precision;
 
 	ft_memset(&flags, 0, sizeof(t_flags));
+	precision = -1;
 	while (is_flag_character(*(++ptr)))
 		set_flags(*ptr, &flags);
 	width = ft_atoi(ptr);
 	while ('0' <= *ptr && *ptr <= '9')
 		ptr++;
-	put_result(c, &flags, width);
+	if (*ptr == '.')
+		precision = ft_atoi(++ptr);
+	put_result(c, &flags, width, precision);
 }
 
-static void	put_result(char c, t_flags *flags, size_t width)
+static void	put_result(char *s, t_flags *flags, size_t width, size_t precision)
 {
+	size_t	space_length;
+
+	precision = min_len(ft_strlen(s), precision);
+	space_length = width - precision;
 	if (flags->left_align == 1)
-		while (--width > 0)
+		while (width-- > 0)
 			write(1, " ", 1);
-	write(1, &c, 1);
+	ft_putstrn(s, precision);
 	if (flags->left_align == 0)
-		while (--width > 0)
+		while (width-- > 0)
 			write(1, " ", 1);
 }
--- printf_utils.c
+++ printf_utils.c
@@ -6,9 +6,11 @@
 /*   By: bena <bena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 16:09:07 by bena              #+#    #+#             */
-/*   Updated: 2023/02/02 16:33:29 by bena             ###   ########.fr       */
+/*   Updated: 2023/02/13 06:19:43 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
 
 #define SIGN_SPACE 1
 #define SIGN_PLUS 2
@@ -31,7 +33,10 @@ int	is_flag_character(const char c)
 void	set_flags(const char c, t_flags *flags)
 {
 	if (c == '-')
+	{
 		flags->left_align = 1;
+		flags->zero_padding = 0;
+	}
 	if (c == '0')
 		flags->zero_padding = 1;
 	if (c == '#')
@@ -40,4 +45,21 @@ void	set_flags(const char c, t_flags *flags)
 		flags->sign = SIGN_SPACE;
 	if (c == '+')
 		flags->sign = SIGN_PLUS;
+}
+
+size_t	min_len(size_t a, size_t b)
+{
+	if (a >= b)
+		return (a);
+	return (b);
+}
+
+void	ft_putstrn(char *s, size_t buffer)
+{
+	char	*ptr;
+
+	ptr = s;
+	while (*ptr && ptr - s < buffer)
+		ptr++;
+	write(1, s, ptr - s);
 }
